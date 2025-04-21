7c7828dca758f4e855b3e7718690dcd674e9beae
Thomas <mosset.thomas@gmail.com>
2025-04-21 20:48:44 +0200

    moved game const into a separate file

---
+++ constants.py
@@ -0,0 +1,22 @@
+# game's constants
+WIDTH = 1280
+HEIGHT = 720
+PIXEL_SIZE = 4 # size an enlarged pixel
+GRAVITY = 1 # makes the bird fall down
+JUMP_STRENGTH = -12 # bird's jump strenght
+BIRD_VERTICAL_SPEED = 0
+COIN_SPAWN_TIME = 180 # a coin every 3 sec (60 FPS * 3)
+COIN_SPEED = 2 # speed of moving coins
+PIPE_HEIGHT = 720
+PIPE_SPEED = 2
+PIPE_DISTANCE = 300
+
+# game's colour
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+YELLOW = (255, 223, 0)
+BROWN = (139, 69, 19)
+DARK_BROWN = (100, 50, 0)
+ORANGE = (255, 165, 0)
+DARK_GRAY = (50, 50, 50)
+SKY_BLUE = (135, 206, 235)
--- flappy.py
+++ flappy.py
@@ -3,6 +3,9 @@ import pygame
 import random
 import time
 
+
+from constants import *
+
 # initialization of pygame
 pygame.init()
 music_on = True
@@ -77,19 +80,6 @@ coin_touched_sound.set_volume(0.2)
 # initialization of the timer (in milliseconds)
 start_time = pygame.time.get_ticks()
 
-# game's constants
-WIDTH = 1280
-HEIGHT = 720
-PIXEL_SIZE = 4 # size an enlarged pixel
-GRAVITY = 1 # makes the bird fall down
-JUMP_STRENGTH = -12 # bird's jump strenght
-BIRD_VERTICAL_SPEED = 0
-COIN_SPAWN_TIME = 180 # a coin every 3 sec (60 FPS * 3)
-COIN_SPEED = 2 # speed of moving coins
-PIPE_HEIGHT = 720
-PIPE_SPEED = 2
-PIPE_DISTANCE = 300
-
 # game's variables
 clock = pygame.time.Clock()
 running = True
@@ -108,19 +98,6 @@ params_menu = False
 music_menu = False
 controls_menu = False
 
-# game's colors (RGB)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-YELLOW = (255, 223, 0)
-BROWN = (139, 69, 19)
-DARK_BROWN = (100, 50, 0)
-ORANGE = (255, 165, 0)
-DARK_GRAY = (50, 50, 50)
-GRAY = (120, 120, 120)
-LIGHT_GRAY = (200, 200, 200)
-SKY_BLUE = (135, 206, 235)
-GREEN_GRASS = (88, 158, 41)
-DARK_GREEN = (51, 92, 36)
 
 # game's images / icons
 timer_img = pygame.image.load("assets/icons/timer.bmp")
